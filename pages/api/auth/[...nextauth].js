06e11ae9a00212195b8f6437b509428fcc72811b
Sam Robbins <samrobbinsgb@gmail.com>
2021-08-22 14:53:51 +0100

    ✨ Authenticate with GitHub and get access token

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+	providers: [
+		Providers.GitHub({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+	],
+	callbacks: {
+		async jwt(token, account) {
+			// Add access_token to the token right after signin
+			if (account?.accessToken) {
+				token.accessToken = account.accessToken;
+			}
+			return token;
+		},
+		async session(session, token) {
+			console.log(token);
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
+});
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function IndexPage() {
+	const [session, loading] = useSession();
+	console.log(session);
 	return (
 		<>
 			<Head>
@@ -12,6 +15,19 @@ export default function IndexPage() {
 			</Head>
 			<div>
 				<h1 className="text-center">Hello, World </h1>
+				{!session && (
+					<>
+						Not signed in <br />
+						<button onClick={() => signIn()}>Sign in</button>
+					</>
+				)}
+				{session && (
+					<>
+						Signed in as {session.user.email} <br />
+						<p>Token -{session.accessToken}</p>
+						<button onClick={() => signOut()}>Sign out</button>
+					</>
+				)}
 			</div>
 		</>
 	);
